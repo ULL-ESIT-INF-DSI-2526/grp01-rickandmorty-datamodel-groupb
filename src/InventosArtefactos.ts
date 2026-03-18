3083f73bca428a444b00453c27012f58c52affc5
EduardoCrugo <alu0101432748@ull.edu.es>
2026-03-18 12:13:38 +0000

    typo

--- src/InventosArtefactos.ts
+++ src/InventosArtefactos.ts
@@ -8,24 +8,24 @@ enum TipoArtefacto {
 }
 
 export class InventosArtefactos extends AtributosComunes {
-  private invetor: string;
+  private inventor: string;
   private tipo: TipoArtefacto;
   private nivelPeligrosidad: number;
 
   constructor(id: string, nombre: string, descripcion: string, inventor: string, tipo: TipoArtefacto, nivelPeligrosidad: number) {
     super(id, nombre, descripcion);
-    this.invetor = this.comprobarVacio(inventor);
+    this.inventor = this.comprobarVacio(inventor);
     this.tipo = this.comprobarTipo(tipo);
     this.nivelPeligrosidad = this.comprobarNivelPeligrosidad(nivelPeligrosidad);
   }
 
   // Getters
-  getInventor(): string { return this.invetor; }
+  getInventor(): string { return this.inventor; }
   getTipo(): TipoArtefacto { return this.tipo; }
   getNivelPeligrosidad(): number { return this.nivelPeligrosidad; }
 
   // Setters
-  setInventor(inventor: string) { this.invetor = this.comprobarVacio(inventor); }
+  setInventor(inventor: string) { this.inventor = this.comprobarVacio(inventor); }
   setTipo(tipo: TipoArtefacto) { this.tipo = this.comprobarTipo(tipo); }
   setNivelPeligrosidad(nivel: number) { this.nivelPeligrosidad = this.comprobarNivelPeligrosidad(nivel); }
 
