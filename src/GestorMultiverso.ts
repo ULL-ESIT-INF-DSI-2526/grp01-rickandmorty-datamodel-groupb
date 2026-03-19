55664d002670e3e3464033f2f5e73f2804e6f972
EduardoCrugo <alu0101432748@ull.edu.es>
2026-03-19 11:44:55 +0000

    ViajeInterdimensional y getViajesInterdimensionales

--- src/GestorMultiverso.ts
+++ src/GestorMultiverso.ts
@@ -4,12 +4,20 @@ import { InventosArtefactos } from "./InventosArtefactos";
 import { Personajes } from "./Personajes";
 import { PlanetasLocalizaciones } from "./PlanetasLocalizaciones";
 
+type ViajeInterdimensional = {
+  personaje: Personajes;
+  dimensionDestino: Dimensiones;
+  fechaViaje: Date;
+  motivo: string;
+};
+
 export class GestorMultiverso {
   private dimensiones: Dimensiones[] = [];
   private personajes: Personajes[] = [];
   private especies: Especies[] = [];
   private planetasLocalizaciones: PlanetasLocalizaciones[] = [];
   private inventosArtefactos: InventosArtefactos[] = [];
+  private viajesInterdimensionales: ViajeInterdimensional[] = [];
 
   // En el constructor hay que cargar la base de datos
   // constructor(db) {}
@@ -35,4 +43,14 @@ export class GestorMultiverso {
 
   // Personajes con mayor numero de versiones alternativas
   // Mismo nombre con diferente id
+
+  // Inventos mas peligrosos desplegados en dimensiones activas
+  // Para sacar la dimension de un artefacto acceder al atributo de dimension de origen del inventor
+  getInventosPeligrososEnDimensionesActivas(): InventosArtefactos[] {
+  }
+
+  // Viajes interdimensionales de un personaje especifico por ID
+  getViajesInterdimensionales(personajeId: string): ViajeInterdimensional[] {
+    return this.viajesInterdimensionales.filter( viaje => viaje.personaje.getId() === personajeId);
+  }
 }
