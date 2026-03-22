a9cd7f83465f430edfb4010c3ee9e36a6801f7d3
Alvaro Martin <alu0101646299@ull.edu.es>
2026-03-22 11:05:57 +0000

    Añadidas funcionalidades faltantes

--- src/InventosArtefactos.ts
+++ src/InventosArtefactos.ts
@@ -1,5 +1,6 @@
 import { AtributosComunes } from "./AtributosComunes";
 import { Personajes } from "./Personajes";
+import { PlanetasLocalizaciones } from "./PlanetasLocalizaciones";
 
 export enum TipoArtefacto {
   ARMA = "arma",
@@ -15,39 +16,36 @@ export interface InventoArtefactoDatos {
   tipo: TipoArtefacto;
   nivelPeligrosidad: number;
   descripcion: string;
+  localizacionDespliegueId: string | null;
 }
 
 export class InventosArtefactos extends AtributosComunes {
   private inventor: Personajes;
   private tipo: TipoArtefacto;
   private nivelPeligrosidad: number;
+  private localizacionDespliegue: PlanetasLocalizaciones | null;
 
-  constructor(id: string, nombre: string, descripcion: string, inventor: Personajes, tipo: TipoArtefacto, nivelPeligrosidad: number) {
+  constructor(id: string, nombre: string, descripcion: string, inventor: Personajes, tipo: TipoArtefacto, nivelPeligrosidad: number, localizacionDespliegue: PlanetasLocalizaciones | null = null) {
     super(id, nombre, descripcion);
     this.inventor = this.comprobarInventor(inventor);
-    this.tipo = this.comprobarTipo(tipo);
+    this.tipo = tipo;
     this.nivelPeligrosidad = this.comprobarNivelPeligrosidad(nivelPeligrosidad);
+    this.localizacionDespliegue = localizacionDespliegue;
   }
 
   // Getters
   getInventor(): Personajes { return this.inventor; }
   getTipo(): TipoArtefacto { return this.tipo; }
   getNivelPeligrosidad(): number { return this.nivelPeligrosidad; }
+  getLocalizacionDespliegue(): PlanetasLocalizaciones | null { return this.localizacionDespliegue; }
 
   // Setters
   setInventor(inventor: Personajes) { this.inventor = this.comprobarInventor(inventor); }
-  setTipo(tipo: TipoArtefacto) { this.tipo = this.comprobarTipo(tipo); }
+  setTipo(tipo: TipoArtefacto) { this.tipo = tipo; }
   setNivelPeligrosidad(nivel: number) { this.nivelPeligrosidad = this.comprobarNivelPeligrosidad(nivel); }
+  setLocalizacionDespliegue(loc: PlanetasLocalizaciones | null) { this.localizacionDespliegue = loc; }
 
   // Comprobaciones
-  comprobarTipo(tipo: string): TipoArtefacto {
-    if (tipo === "arma" || tipo === "dispositivo de viaje" || tipo === "biotecnología" || tipo === "objeto cotidiano absurdo") {
-      return tipo as TipoArtefacto;
-    } else {
-      throw new Error("El tipo de artefacto sólo puede ser arma/dispositivo de viaje/biotecnología/objeto cotidiano absurdo");
-    }
-  }
-
   comprobarInventor(inventor: Personajes): Personajes {
     if (inventor === null || inventor === undefined) {
       throw new Error("El inventor no puede estar vacío");
